0bdc72c31b278b55da9fe2e3e6b12c826ab42fe9
gdefroyenne <72787707+gdefroyenne@users.noreply.github.com>
2020-10-13 17:35:55 +0200

    update

--- src/App.js
+++ src/App.js
@@ -65,6 +65,46 @@ function App() {
           <a href="#"><img src="hp-XV-france-1400.jpg" alt="" class="img-fluid w-100" /></a>
         </div>
       </div>
+      <div class="row">
+        <div class="col-md-6">
+          <a href="#"><img src="push-neree-960.jpg" alt="" class="img-fluid w-100" /></a>
+        </div>
+        <div class="col-md-6">
+          <a href="#"><img src="hp-ton-sur-ton-960.jpg" alt="" class="img-fluid w-100" /></a>
+        </div>
+      </div>
+      <div class="row"><span>En ce moment</span></div>
+      <div class="row"><span>Un vestiaire idéal pour l'automne</span></div>
+      <div class="row">
+        <div class="col-sm">
+          <a href="#"><img src="m_2021521_1.jpg" class="mx-auto d-block" width="300" height="300" alt=""/></a>
+          <div class="row center"><a href="#"><span>Sweat Essentiels</span></a></div>
+          <div class="row center"><span>79€</span></div>
+        </div>
+        <div class="col-sm">
+          <a href="#"><img src="m_2020815_1.jpg" class="mx-auto d-block" width="300" height="300" alt=""/></a>
+          <div class="row center"><a href="#"><span>Bomber Essentiels</span></a></div>
+          <div class="row center"><span>139€</span></div>
+        </div>
+        <div class="col-sm">
+          <a href="#"><img src="m_2020811_1.jpg" class="mx-auto d-block" width="300" height="300" alt=""/></a>
+          <div class="row center"><a href="#"><span>Pantalon Essentiels</span></a></div>
+          <div class="row center"><span>139€</span></div>
+        </div>
+        <div class="col-sm">
+          <a href="#"><img src="m_2020668_1.jpg" class="mx-auto d-block" width="300" height="300" alt=""/></a>
+          <div class="row center"><a href="#"><span>Prestige</span></a></div>
+          <div class="row center"><span>145€</span></div>
+        </div>
+      </div>
+      <nav class="navbar navbar-light bg-light justify-content-center">
+        <form class="form-inline">
+          <button type="button" class="btn btn-light">Light</button>
+          <button type="button" class="btn btn-light">Light</button>
+          <button type="button" class="btn btn-light">Light</button>
+          <button type="button" class="btn btn-light">Light</button>
+        </form>
+      </nav> 
     </div>
   );
 }
